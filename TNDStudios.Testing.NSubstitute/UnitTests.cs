5e14de8aaeea328a836cba5c8bf17267117edb27
TNDStudios <info@thenakeddeveloper.com>
2019-01-23 00:36:53 +0000

    Initial Check In

---
+++ TNDStudios.Testing.NSubstitute/UnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TNDStudios.Testing.NSubstitute
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+
+        }
+    }
+}
